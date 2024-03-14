ee11668cc613e523dd10d208533fb31cda232d0b
Your Name <you@example.com>
2024-03-14 19:32:19 +0200

    checkss

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,10 +6,10 @@ import logo from "../images/לוגו עיגול.png"
 import Button2 from './Button2';
 const StickyNavbar = () => {
   return <>
-       {window.innerWidth < 850 && <div className={styles.smallNav}><Button2 text="לעוד פרטים" msg="אני מעוניין לקבל פרטים נוספים על ההכנה שלכם \u269C\ufe0f"/>
+       {window.innerWidth < 850 && <div className={styles.smallNav}><Button2 text="לעוד פרטים" msg="אני מעוניין לקבל פרטים נוספים על ההכנה שלכם 🥷🏻"/>
        {window.innerWidth >= 550 && window.innerWidth<=850 && <img className={styles.logo} src={logo} alt="מחר מלחמה"/> }</div>}       
        {window.innerWidth > 850 &&  <div className={styles.stickyNavbar}>
-       <div className={styles.navLogo}><Button msg="אני מעוניין לקבל פרטים נוספים על ההכנה שלכם \u269C\ufe0f"/></div>
+       <div className={styles.navLogo}><Button msg="אני מעוניין לקבל פרטים נוספים על ההכנה שלכם 🥷🏻"/></div>
        <ul className={styles.navLinks}>
    
 
