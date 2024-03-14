3914f026bbb361a540f4f1a4b9c132dbd211bfc5
Your Name <you@example.com>
2024-03-14 19:23:13 +0200

    checks

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,7 +6,7 @@ import logo from "../images/לוגו עיגול.png"
 import Button2 from './Button2';
 const StickyNavbar = () => {
   return <>
-       {window.innerWidth < 850 && <div className={styles.smallNav}><Button2 text="לעוד פרטים" msg="היי שחריה אני אשמח לדעת עוד על"/>
+       {window.innerWidth < 850 && <div className={styles.smallNav}><Button2 text="לעוד פרטים" msg="אני מעוניין לקבל פרטים נוספים על ההכנה שלכם \u269C\ufe0f"/>
        {window.innerWidth >= 550 && window.innerWidth<=850 && <img className={styles.logo} src={logo} alt="מחר מלחמה"/> }</div>}       
        {window.innerWidth > 850 &&  <div className={styles.stickyNavbar}>
        <div className={styles.navLogo}><Button msg="אני מעוניין לקבל פרטים נוספים על ההכנה שלכם \u269C\ufe0f"/></div>
